25cc3f17858372ad3c2ec12d899ea95b565ad6a6
kornatskyi <bohdan.kornatskyi@gmail.com>
2021-05-21 19:54:24 -0700

    made calculator moveble

--- src/components/calculator/Calculator.js
+++ src/components/calculator/Calculator.js
@@ -1,31 +1,56 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './calculator.scss'
 import Display from "./Display";
 import ButtonPanel from "./ButtonPanel";
 import calculate from "../../logic/calculate";
+import './calculator.scss'
 
 
 
 export default class Calculator extends React.Component {
-    state = {
+  constructor(props) {
+    super(props);
+
+    this.state = {
       total: null,
       next: null,
       operation: null,
+      coordinateX: 0,
+      coordinateY: 0
     };
-
-    
-  
-    handleClick = buttonName => {
-      this.setState(calculate(this.state, buttonName));
-    };
-  
-    render() {
-      return (
-        <div className="component-app">
-          <Display value={this.state.next || this.state.total || "0"} />
-          <ButtonPanel clickHandler={this.handleClick} />
-        </div>
-      );
-    }
+    this.drag = this.drag.bind(this)
   }
-  
+
+  func = (e) => {
+    this.setState(state => ({
+      coordinateX: e.clientX,
+      coordinateY: e.clientY
+    }))
+  }
+
+  drag(e) {
+    if (e.type === "mousedown") {
+      document.addEventListener('mousemove', this.func)
+    } else {
+      document.removeEventListener('mousemove', this.func)
+
+    }
+
+  }
+
+  handleClick = buttonName => {
+    this.setState(calculate(this.state, buttonName));
+  };
+
+  render() {
+
+
+    return (
+      <div className="calculator-container" style={{ top: this.state.coordinateY + "px", left: this.state.coordinateX + "px" }}>
+        <div onMouseDown={(e) => this.drag(e)} onMouseUp={(e) => this.drag(e)} className="titlebar"></div>
+        <Display value={this.state.next || this.state.total || "0"} />
+        <ButtonPanel clickHandler={this.handleClick} />
+      </div >
+    );
+  }
+}
