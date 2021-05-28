8b99d62f4782708f3fa214e6bb2eedddb77b1f2d
kornatskyi <bohdan.kornatskyi@gmail.com>
2021-05-27 19:46:36 -0700

    convert mathML to LaTeX end then render by KaTeX

--- src/components/pages/challenge_page/challege_description/ChallengePageChallengeDescription.jsx
+++ src/components/pages/challenge_page/challege_description/ChallengePageChallengeDescription.jsx
@@ -1,24 +1,39 @@
 import React from "react";
-import './challenge-page-challenge-description.scss'
+import "./challenge-page-challenge-description.scss";
+
+const Mathml2latex = require('mathml-to-latex');
+const katex = require('katex');
 
 export default function ChallengePageChallengeDescription(props) {
   if (props.loading) {
     return (
       <div className="challenge-page-challenge-description-constainer">
         <div>Waiting for challenge load...</div>
+
       </div>
     );
   } else {
+
     return (
       <div className="challenge-page-challenge-description-constainer">
         <h2>Task</h2>
         <p>{props.text}</p>
         <img src={props.host + props.images} alt="" />
         <div className="formula">
-          <span dangerouslySetInnerHTML={{__html:props.formulas["formula1"]}}>
-          </span>
+          <script
+            defer
+            type="text/javascript"
+            src="http://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML"
+          ></script>
+          <span
+            // dangerouslySetInnerHTML={{ __html: props.formulas["formula1"] }}
+            dangerouslySetInnerHTML={{ __html: katex.renderToString(Mathml2latex.convert(props.formulas["formula1"])) }}
+          ></span>
+          {console.log(katex.renderToString(Mathml2latex.convert(props.formulas["formula1"])) )}
+          
         </div>
-
+  
+       
         <div>To find: {props.shortTask}</div>
         <div>Answer format: {props.answerType} </div>
       </div>
