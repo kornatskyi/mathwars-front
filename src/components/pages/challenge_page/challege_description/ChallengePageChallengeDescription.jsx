26b8fb15693d48e958e1eb26e781b44213aeeb52
kornatskyi <bohdan.kornatskyi@gmail.com>
2021-06-10 21:33:42 -0700

    test new math formats

--- src/components/pages/challenge_page/challege_description/ChallengePageChallengeDescription.jsx
+++ src/components/pages/challenge_page/challege_description/ChallengePageChallengeDescription.jsx
@@ -4,7 +4,12 @@ import "./challenge-page-challenge-description.scss";
 //This is working for some reason I should make something more consistent to display math formulas
 import MathJax from "react-mathjax-preview";
 
+const latex = "`\\text { Simplify the radical } \\sqrt{147} \\text { to the simplest form. }`";
+// const asciimath = '`int(t^(2)-(5)/(2t)+3sin(2t)-cos(t)-2)dt.`'
+// const asciimath = "` Simplify the radical 'sqrt(147)' to the simplest form.`"
+const asciimath = "`\" Simplify the radical \"sqrt(147)\" to the simplest form.\"`"
 
+const mathMl = "<math><mtext>\u00a0Simplify the radical\u00a0</mtext><msqrt><mrow><mn>147</mn></mrow></msqrt><mtext>\u00a0to the simplest form.\u00a0</mtext></math>"
 
 export default function ChallengePageChallengeDescription(props) {
   if (props.loading) {
@@ -17,7 +22,8 @@ export default function ChallengePageChallengeDescription(props) {
     return (
       <div className="challenge-page-challenge-description-constainer">
         <h2>Task</h2>
-        <MathJax math={props.body} />
+        {/* <MathJax math={props.body} /> */}
+        <MathJax math={mathMl} />
         <img src={props.host + props.images} alt="" />
 
         <div className="to-find">To find: {props.shortTask}</div>
